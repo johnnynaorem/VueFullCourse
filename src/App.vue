b69a19ddcb139b533feecae11e4093acc22e5fcb
johnnynaorem <johnnynaorem7@gmail.com>
2025-02-04 13:49:49 +0530

    add dynamic rendering for select

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,28 @@
   <input type="radio" id="two" v-model="picked" value="Two" />
   <label for="two">Two</label>
 
-  <h1>Select</h1>
-  <div>Selected: {{ selected }}</div>
-  <select v-model="selected" multiple>
+  <h1>Single Select</h1>
+  <div>Selected: {{ singleSelected }}</div>
+  <select v-model="singleSelected">
     <option>1</option>
     <option>2</option>
     <option>3</option>
+  </select>
+
+  <h1>Multiple Select</h1>
+  <div>Selected: {{ multipleSelected }}</div>
+  <select v-model="multipleSelected" multiple>
+    <option>1</option>
+    <option>2</option>
+    <option>3</option>
+  </select>
+
+  <h1>Dynamic Rendering</h1>
+  <div>Selected: {{ multipleSelected }}</div>
+  <select v-model="multipleSelected" multiple>
+    <option v-for="(option, i) in options" :value="option.value" :key="i">
+      {{ option.text }}
+    </option>
   </select>
 </template>
 
@@ -29,7 +45,13 @@ import { ref } from 'vue';
 
 const checkNames = ref([]);
 const picked = ref('');
-const selected = ref()
+const singleSelected = ref()
+const multipleSelected = ref()
+const options = ref([
+  { text: 'One', value: 'A' },
+  { text: 'Two', value: 'B' },
+  { text: 'Three', value: 'C' }
+])
 
 </script>
 
